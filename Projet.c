02063a7ed92289149452c2d6116ed5355613aa82
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2022-01-14 20:29:25 +0100

    Add files via upload

---
+++ Projet.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+//commande de compil gcc Projet.c -lm -o Projet
+int creationfichier(FILE*,unsigned long ,int , unsigned long , unsigned long, int,int);
+
+int main(int argc, char **argv)
+{   
+    if (argc!=3)
+    {
+        printf("Nombre d'argument incorrect\n"); 
+        exit(1);
+    }
+    unsigned long U0 = 0;
+    int n=0;
+    unsigned long maxtmp = 0;
+    unsigned long U0tmp = 0;
+    char* pointeurdefin;
+    U0 = strtoul(argv[1], &pointeurdefin, 10);
+    if(U0==0)
+    {
+        puts("U0 n'est pas un entier superieur a 0");
+        exit(2);
+    } 
+    U0tmp = U0;
+    unsigned long  altitude = 0;
+    FILE* fichier; 
+	char* nomfichier = NULL;
+	nomfichier = argv[2];
+	int veriff;
+	fichier = (fopen((strcat(nomfichier, ".dat")),"w+"));
+    int volmax = 0;
+    if(fichier == NULL)
+		{                
+			fprintf(stderr,"Impossible de creer le fichier \n");
+			exit(3);
+		}
+	/*else{
+		fprintf(stderr,"Fichier bien crée\n");
+		} */
+    creationfichier(fichier,U0,n,maxtmp,U0tmp,altitude, volmax);
+}
+
+int creationfichier(FILE* fichier,unsigned long x,int i, unsigned long max, unsigned long tmp, int inf,int maxpourvolalt)
+{
+    if(x>=tmp) inf++;
+    if(x<tmp)
+    {
+        if(maxpourvolalt<inf) maxpourvolalt =inf;
+        inf =0;
+    }
+    if(max<x) max= x;
+    fprintf(fichier,"%d %lu\n",i,x);
+    if(x==1)
+    {
+        fprintf(fichier,"\n\n%lu %lu\n\n\n%lu %d\n\n\n%lu %d\n",tmp, max, tmp, i, tmp, maxpourvolalt-1);
+        return 0;
+    }
+
+    if (x%2==0) creationfichier(fichier,x/2,i+1,max, tmp, inf, maxpourvolalt);
+    else creationfichier(fichier,x*3+1,i+1,max,tmp, inf, maxpourvolalt);
+
+}
---
+++ Projetbash.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+main() {
+test "$@"
+creation "$@"
+}
+
+test(){
+if [ "$1" == "-h" ]; then
+ aide
+ exit 0
+fi
+if [ "$#" -ne 2 ] && ! [ "$1" == "-h" ] ; then
+ echo "Le nombre d'arguments est invalide"
+ aide
+ exit 1
+fi
+if ! [ "$1" == "-h" ] && ! [[ "$1" =~ ^-?[0-9]+$ ]]; then
+ echo "Le premier argument n est pas un entier"
+ aide
+ exit 2
+fi
+if ! [ "$1" == "-h" ] && ! [[ "$2" =~ ^-?[0-9]+$ ]]; then
+ echo "Le second argument n est pas un entier"
+ aide
+ exit 3
+fi
+if ! [ "$1" == "-h" ] && [ "$1" -gt "$2" ]; then
+ echo "Le premier argument est superieur au second"
+ aide
+ exit 4
+fi
+}
+
+aide(){
+echo "Blabla aide"
+}
+
+creation(){
+gcc Projet.c -o Projet
+mkdir -p resultats
+cd resultats
+mkdir -p tmp
+cd tmp
+x="$1"
+y="$2"
+for (( i=x; i<=y; i++ ))
+do ../../Projet "$i" "f$i"
+error=$?
+if [ "$error" -ne 0 ] ; then
+ echo "Probleme dans l'execution du programme en c, code d'erreur = "$error""
+ aide
+ exit 5
+fi
+done
+gnuplot <<EOF
+set terminal jpeg 
+set output "../vols$x-$y.jpg"
+set title 'Un en fonction de n'
+set xlabel 'n'
+set ylabel 'Un'
+plot for [t = $x : $y : 1] 'f'.t.'.dat' i 0 u 1:2 with lines title ''
+set output
+set terminal pop
+replot
+EOF
+gnuplot <<EOF
+set terminal jpeg 
+set output "../altmax$x-$y.jpg"
+set title 'Altitude maximum pour chaque U0'
+set xlabel 'U0'
+set ylabel 'Altitude max'
+plot for [t = $x : $y : 1] 'f'.t.'.dat' i 1 u 1:2 w p ls 1 title '' 
+EOF
+gnuplot <<EOF
+set terminal jpeg 
+set output "../dureedevol$x-$y.jpg"
+set title 'Durée de vol pour chaque U0'
+set xlabel 'U0'
+set ylabel 'Durée de vol'
+plot for [t = $x : $y : 1] 'f'.t.'.dat' i 2 u 1:2 w p ls 1 title '' 
+EOF
+gnuplot <<EOF
+set terminal jpeg 
+set output "../dureevolalt$x-$y.jpg"
+set title 'Durée de vol en altitude pour chaque U0'
+set xlabel 'U0'
+set ylabel 'Durée de vol en altitude'
+plot for [t = $x : $y : 1] 'f'.t.'.dat' i 3 u 1:2 w p ls 1 title '' 
+EOF
+cd ..
+xdg-open ./vols$x-$y.jpg
+xdg-open ./altmax$x-$y.jpg
+xdg-open ./dureedevol$x-$y.jpg
+xdg-open ./dureevolalt$x-$y.jpg
+rm -rf tmp
+cd ..
+}
+main "$@"
+
