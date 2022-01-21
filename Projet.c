626a231f66a4073e485a99f0208794d289a6e54f
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2022-01-21 20:23:58 +0100

    Enlever math. et verif si ca marche

--- Projet.c
+++ Projet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+#include <math.h>   #enlevercellela
 //commande de compil gcc Projet.c -lm -o Projet
 int creationfichier(FILE*,unsigned long ,int , unsigned long , unsigned long, int,int);
 
