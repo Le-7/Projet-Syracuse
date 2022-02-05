bd85b0ce816fd6c0967a45d77e1d56a4c5bb6088
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2022-02-05 14:15:56 +0100

    Faire texte aide + readme(pas oublier imagequick)

--- Projet.c
+++ Projet.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//commande de compil gcc Projet.c -o Projet
 int creationfichier(FILE*,unsigned long ,int , unsigned long , unsigned long, int,int);
+//appel des bibliotheques nécessaire et prototype de fonction
 int main(int argc, char **argv)
-{   
+{       //on verifie que le programme a bien été appelé sans erreur (trop d'argument ou pas un entier)
     if (argc!=3)
     {
         printf("Nombre d'argument incorrect\n"); 
         exit(1);
     }
+        //déclaration des variables
     unsigned long U0 = 0;
     int n=0;
     unsigned long maxtmp = 0;
     unsigned long U0tmp = 0;
     char* pointeurdefin;
-    U0 = strtoul(argv[1], &pointeurdefin, 10);
+    U0 = strtoul(argv[1], &pointeurdefin, 10); //fonction de string.h "string to unsigned long"(convertir)
     if(U0==0)
     {
         puts("U0 n'est pas un entier superieur a 0");
@@ -27,7 +28,7 @@ int main(int argc, char **argv)
 	char* nomfichier = NULL;
 	nomfichier = argv[2];
 	int veriff;
-	fichier = (fopen((strcat(nomfichier, ".dat")),"w+"));
+	fichier = (fopen((strcat(nomfichier, ".dat")),"w+")); //on ouvre un fichier .dat en droit d'écriture
     int volmax = 0;
     if(fichier == NULL)
 		{                
@@ -37,26 +38,27 @@ int main(int argc, char **argv)
 	/*else{
 		fprintf(stderr,"Fichier bien crée\n");
 		} */
-    creationfichier(fichier,U0,n,maxtmp,U0tmp,altitude, volmax);
+    creationfichier(fichier,U0,n,maxtmp,U0tmp,altitude, volmax); //appel de la fonction récursive
 }
 
+//la ca devient compliqué a expliquer
 int creationfichier(FILE* fichier,unsigned long x,int i, unsigned long max, unsigned long tmp, int inf,int maxpourvolalt)
 {
-    if(x>=tmp) inf++;
+    if(x>=tmp) inf++; //pour le calcul de vol en altitude
     if(x<tmp)
     {
-        if(maxpourvolalt<inf) maxpourvolalt =inf;
-        inf =0;
+        if(maxpourvolalt<inf) maxpourvolalt =inf; //on enregistre le maximum s'il l'est
+        inf =0;  //on remet a 0 s'il retombe en dessous de la valeur de départ
     }
-    if(max<x) max= x;
-    fprintf(fichier,"%d %lu\n",i,x);
-    if(x==1)
+    if(max<x) max= x;   //pour calculer le max
+    fprintf(fichier,"%d %lu\n",i,x);  //on écrit dans le fichier la nieme etape et le Un
+    if(x==1) //condition d'arret
     {
-        if(maxpourvolalt<=0)maxpourvolalt =1;
-        fprintf(fichier,"\nAltitudemax=%lu\nDureevol=%d\ndureealtitude=%d\n",max,i, maxpourvolalt-1);
+        if(maxpourvolalt<=0)maxpourvolalt =1;  //pour pas avoir de probleme dans la suite avec un O
+        fprintf(fichier,"\nAltitudemax=%lu\nDureevol=%d\ndureealtitude=%d\n",max,i, maxpourvolalt-1);  //si x=1 alors on a fini, on ecrit a la fin du  fichier ce qui nous interesse 
         return 0;
     }
-
+    //appels recursifs
     if (x%2==0) creationfichier(fichier,x/2,i+1,max, tmp, inf, maxpourvolalt);
     else creationfichier(fichier,x*3+1,i+1,max,tmp, inf, maxpourvolalt);
 
--- Projetbash.sh
+++ Projetbash.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 main() {
-test "$@"
+test "$@"      #fonction principale
 creation "$@"
 }
-
-test(){
+test(){                          #fonction de test préliminaire
 if [ "$1" == "-h" ]; then
- aide
+ aide                            #on regarde si l'utilisateur a demandé de l'aide (appel a la fonction d'aide)
  exit 0
 fi
 if [ "$#" -ne 2 ] && ! [ "$1" == "-h" ] ; then
  echo "Le nombre d'arguments est invalide"
- aide
+ aide                           #on regarde s'il y a bien que 2 arguments
  exit 1
 fi
 if ! [ "$1" == "-h" ] && ! [[ "$1" =~ ^-?[0-9]+$ ]]; then
  echo "Le premier argument n est pas un entier"
- aide
+ aide                           #on verifie que les arguments sont bien des entiers
  exit 2
 fi
 if ! [ "$1" == "-h" ] && ! [[ "$2" =~ ^-?[0-9]+$ ]]; then
@@ -26,28 +25,28 @@ if ! [ "$1" == "-h" ] && ! [[ "$2" =~ ^-?[0-9]+$ ]]; then
 fi
 if ! [ "$1" == "-h" ] && [ "$1" -gt "$2" ]; then
  echo "Le premier argument est superieur au second"
- aide
+ aide                           #si le premier est inférieur au second le programme n'a plus de sens car pas d'intervalle
  exit 4
 fi
 }
 
 aide(){
-echo "Blabla aide"
+echo "Blabla aide"              #fonction d'affichage de l'aide
 }
 
-creation(){
-gcc Projet.c -o Projet
-mkdir -p resultats
+creation(){                     #fonction principale de création des graphiques
+gcc Projet.c -o Projet          #on compile le programme en c
+mkdir -p resultats              
 cd resultats
-mkdir -p tmp
-cd tmp
+mkdir -p tmp                    #on crée un dossier temporaire dans le dossier resultats 
+cd tmp                          #on se place dedans pour la création des fichiers
 x="$1"
 y="$2"
 echo "Creation des fichiers sources ..."
 for (( i=x; i<=y; i++ ))
-do ../../Projet "$i" "f$i"
+do ../../Projet "$i" "f$i"                  #boucle de création avec le programme en c
 error=$?
-if [ "$error" -ne 0 ] ; then
+if [ "$error" -ne 0 ] ; then  #si une erreur est detectée dans le programme en c on renvoie le code d'erreur
  echo "Probleme dans l'execution du programme en c, code d'erreur = "$error""
  aide
  exit 5
@@ -55,12 +54,12 @@ fi
 done
 touch toto.dat
 touch tata.dat
-touch titi.dat
+touch titi.dat          #on crée 3 fichiers temporaire pour les différents graphiques
 touch synthese-$x-$y.txt  
-moy1=0 moy2=0 moy3=0
-min1=2147483647 min2=2147483647 min3=2147483647
+moy1=0 moy2=0 moy3=0     #declaration des variables pour la synthese
+min1=2147483647 min2=2147483647 min3=2147483647     #arbitrairement grande (max d'un int)
 max1=0 max2=0 max3=0
-for (( i=x; i<=y; i++ ))
+for (( i=x; i<=y; i++ ))        #boucle d'eciture dans les 3 fichiers précédents
 do 
 echo "${i} $(cat 'f'$i'.dat' | tail -1 | grep -Eo '[0-9]{1,}')" >> toto.dat
 moy1=$(($(cat 'f'$i'.dat' | tail -1 | grep -Eo '[0-9]{1,}')+$moy1))
@@ -90,8 +89,8 @@ elif [ $(cat 'f'$i'.dat' | tail -3 | head -n +1 | grep -Eo '[0-9]{1,}') -lt $min
  #echo "Altitude max min = $min3"
 fi
 done
-i=$(($y-$x))
-echo "Durée de vol en altitude minimum = $min1" >> synthese-$x-$y.txt 
+i=$(($y-$x)) 
+echo "Durée de vol en altitude minimum = $min1" >> synthese-$x-$y.txt   #on ecrit toutes les informations dans la synthese
 echo "Durée de vol en altitude maximum = $max1" >> synthese-$x-$y.txt 
 echo "Durée de vol en altitude moyen (arrondi a l'entier superieur) = $(($moy1/$i))" >> synthese-$x-$y.txt 
 echo "Durée de vol minimum  = $min2" >> synthese-$x-$y.txt
@@ -100,17 +99,17 @@ echo "Durée de vol moyen (arrondi a l'entier superieur) = $(($moy2/$i))" >> syn
 echo "Altitude max minimum = $min3" >> synthese-$x-$y.txt 
 echo "Altitude max maximum = $max3" >> synthese-$x-$y.txt 
 echo "Altitude max moyenne (arrondie a l'entier superieur) = $(($moy3/$i))" >> synthese-$x-$y.txt 
-cp synthese-$x-$y.txt ../synthese-$x-$y.txt #on ecrase le fichier s'il existe deja
+cp synthese-$x-$y.txt ../synthese-$x-$y.txt #on ecrase le fichier s'il existe deja (d'ou le cp)
 echo "Fichiers sources bien crees"
-echo "Creation des graphiques ..."
-gnuplot <<EOF
+echo "Creation des graphiques ..." 
+gnuplot <<EOF     
 set terminal jpeg 
 set output "../vols$x-$y.jpg"
 set title 'Un en fonction de n'
 set xlabel 'n'
 set ylabel 'Un'
 plot for [t = $x : $y : 1] 'f'.t.'.dat' i 0 u 1:2 with lines title ''
-EOF
+EOF    #on execute des commandes dans gnuplot jusqu'au signal de fin(EOF) pour creer les differents graphiques
 gnuplot <<EOF
 set terminal jpeg 
 set output "../altmax$x-$y.jpg"
@@ -137,13 +136,13 @@ plot "toto.dat" u 1:2 with lines title ''
 EOF
 echo "Graphiques bien crees les voici :"
 cd ..
-display ./vols$x-$y.jpg &
+display ./vols$x-$y.jpg &     #on affiche les images avec display de imagemagick pour un affichage plus convenable: https://imagemagick.org/index.php
 display ./altmax$x-$y.jpg &
 display ./dureedevol$x-$y.jpg &
 display ./dureevolalt$x-$y.jpg &
-xdg-open ./synthese-$x-$y.txt
-rm -rf tmp
-cd ..
+xdg-open ./synthese-$x-$y.txt   #pour la synthese en txt xdg-open suffit 
+rm -rf tmp  #on supprime le dossier temporaire
+cd ..  #on se replace dans le repertoire de base
 }
-main "$@"
+main "$@"  #on appel la fonction main avec la liste de tous les arguments.
 
